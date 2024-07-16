b1164ef4559e86a649eedad06984b4c8a5af512e
KFC <kfclts@gmail.com>
2024-07-16 12:09:14 +0800

    create sales-person-list and SalesPerson

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+  // create an array if object
+  salespPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "aa@gmail.com", 50000),
+    new SalesPerson("John", "Doe", "jd@gmail.com", 40000),
+    new SalesPerson("Claire", "Murphy", "cm@gmail.com", 90000),
+    new SalesPerson("Mai", "Truonh", "mt@gmail.com", 60000),
+  ]
+  
+}
